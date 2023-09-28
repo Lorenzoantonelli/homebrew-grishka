baafabdf2d8e2f62cb88a2b0a11ce5a7fc6afbeb
Grishka <git@grishka.me>
2023-09-28 06:48:37 +0300

    NearDrop 2.0.2

--- Casks/neardrop.rb
+++ Casks/neardrop.rb
@@ -1,6 +1,6 @@
 cask "neardrop" do
-  version "2.0.1"
-  sha256 "560c15a9179d57dbecbad514734431e1cb3d98f48d1f4d3d3694bc7dd89c382b"
+  version "2.0.2"
+  sha256 "b270f084d2a25d3c3f7302112739898dbf6e1fd48b4b560d3f80b04c482f95a8"
 
   url "https://github.com/grishka/NearDrop/releases/download/v#{version}/NearDrop.app.zip"
   name "NearDrop"
